7252b01a0aae48fc5433b5e3c7571a9f59b8a1ef
fabiofalavinha <fabio.falavinha@gmail.com>
2015-06-17 21:08:32 -0300

    organized code

--- src/main/java/org/religion/umbanda/tad/Application.java
+++ src/main/java/org/religion/umbanda/tad/Application.java
@@ -1,8 +1,7 @@
 package org.religion.umbanda.tad;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Locale;
