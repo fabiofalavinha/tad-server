3140d64dfcde208d8b1200eabe4e5800e9d3285f
fabiofalavinha <fabio.falavinha@gmail.com>
2015-07-03 16:53:03 -0300

    - fixed charset configuration

--- src/main/java/org/religion/umbanda/tad/service/filter/CharEncodingConfigurationFilter.java
+++ src/main/java/org/religion/umbanda/tad/service/filter/CharEncodingConfigurationFilter.java
@@ -8,18 +8,24 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class CorsServiceFilter implements Filter {
+public class CharEncodingConfigurationFilter implements Filter {
+
+    private static final String defaultCharset = "iso-8859-1";
 
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        final HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        //final HttpServletResponse response = (HttpServletResponse) res;
+        //Charset charset;
+        //try {
+        //    charset = Charset.forName(defaultCharset);
+        //} catch (UnsupportedCharsetException ex) {
+        //    ex.printStackTrace();
+        //    charset = Charset.defaultCharset();
+        //}
+        //response.setCharacterEncoding(charset.displayName());
+        res.setContentType("application/json; charset=iso-8859-1");
         chain.doFilter(req, res);
     }
 
