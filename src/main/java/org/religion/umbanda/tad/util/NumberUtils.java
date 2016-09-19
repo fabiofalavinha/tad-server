48327ab3eac0a5e4a6f33d8eb09f8b2acd357bce
Fabio Falavinha <fabio.falavinha@gmail.com>
2016-09-19 13:26:22 -0300

    fixed parse string to BigDecimal

--- src/main/java/org/religion/umbanda/tad/util/NumberUtils.java
+++ src/main/java/org/religion/umbanda/tad/util/NumberUtils.java
@@ -1,27 +1,13 @@
 package org.religion.umbanda.tad.util;
 
 import java.math.BigDecimal;
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.text.ParseException;
-import java.util.Locale;
 
 public final class NumberUtils {
 
     public static BigDecimal parseNumber(String number) {
-        try {
-            number = number.replace(".", "");
-            number = number.replace(",", ".");
-            DecimalFormatSymbols symbols = new DecimalFormatSymbols(new Locale("pt", "BR"));
-            symbols.setGroupingSeparator(',');
-            symbols.setDecimalSeparator('.');
-            String pattern = "#0.0#";
-            DecimalFormat decimalFormat = new DecimalFormat(pattern, symbols);
-            decimalFormat.setParseBigDecimal(true);
-            return (BigDecimal) decimalFormat.parse(number);
-        } catch (ParseException e) {
-            throw new IllegalArgumentException(String.format("Error parsing number [%s]", number), e);
-        }
+        number = number.replace(".", "");
+        number = number.replace(",", ".");
+        return new BigDecimal(number);
     }
 
     private NumberUtils() {
