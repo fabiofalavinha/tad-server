78f3c2cff39f4a03ff62f871310be7546c7ec053
Fabio Falavinha <fabio.falavinha@gmail.com>
2016-09-29 17:18:08 -0300

    organized code

--- src/main/java/org/religion/umbanda/tad/service/vo/NewsletterUserVO.java
+++ src/main/java/org/religion/umbanda/tad/service/vo/NewsletterUserVO.java
@@ -5,11 +5,19 @@ public class NewsletterUserVO {
     private String name;
     private String email;
 
-    public String getName() { return name;}
+    public String getName() {
+        return name;
+    }
 
-    public void setName(String name) { this.name = name;}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public String getEmail() { return email;}
+    public String getEmail() {
+        return email;
+    }
 
-    public void setEmail(String email) { this.email = email;}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
